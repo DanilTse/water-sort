8753db6200421a65085c862356f40b36c147a3eb
DanilTse <danil.tse1265@gmail.com>
2021-10-05 01:01:21 +0300

    bottle component, game screen

---
+++ src/components/Bottle.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="bottle" @click="bottleClick()">
+      <div class="bottle-inner">
+          <span v-for="(color, key) in data" :key="key" :class="color"></span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Bottle',
+  props: ['data'],
+  data() {
+    return {
+      msg: "message"
+    }
+  },
+  computed:{
+
+  },
+  created: {
+
+  },
+  methods: {
+    bottleClick(){
+        console.log("click")
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.bottle{
+   display: block;
+   width: 30px;
+   height: 100px;
+   margin: 20px;
+   border-width: 0 2px 2px 2px;
+   border-radius: 0 0 15px 15px;
+   overflow: hidden;
+   border-style: solid;
+   border-color: #000;
+   cursor: pointer;
+   transition: all .15s ease;
+   &:hover{
+       transform: translateY(-15px);
+   }
+   &-inner{
+        display: flex;
+        flex-direction: column;
+        height: 100px;
+        background: #fff;
+        span{
+            display: block;
+            height: 100%;
+            width: 100%;
+            &.blue{
+                background: blue;
+            }
+            &.red{
+                background: red;
+            }
+            &.yellow{
+                background: yellow;
+            }
+            &.green{
+                background: green;
+            }
+        }
+   }
+   
+}
+</style>
---
+++ src/components/GameScreen.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="game-wrap">
+      <bottle v-for="(bottle, key) in bottles" :key="key" :data="bottle"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Bottle from './Bottle.vue'
+
+export default {
+  components: { Bottle },
+  name: 'GameScreen',
+  data() {
+    return {
+      level: 1,
+      bottlesCount: 3,
+      bottleSize: 4,
+      bottles: [],
+      colors: ['red', 'blue', 'green', 'yellow'],
+      emptyBottle: [false, false, false ,false]
+    }
+  },
+  computed:{
+
+  },
+  created() {
+    let i = 0;
+    while (i < this.bottlesCount) {
+      this.bottles.push(this.createBottle())
+      i++
+    }
+    this.bottles.push(this.emptyBottle)
+  },
+  methods: {
+    createBottle(){
+        let i = 0
+        let bottle = []
+        while(i < this.bottleSize){
+          i++
+          bottle.push(this.colors[Math.floor(Math.random() * this.colors.length)])
+        }
+      return bottle
+    },
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.game-wrap{
+  display: flex;
+}
+</style>
